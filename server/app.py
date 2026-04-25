dbb3375580914ff78d3e684b2a6f3f22802e70ef
Rich <rchppls@gmail.com>
2026-04-25 16:23:33 -0600

    Continued setup: Pipenv, Flask app skeleton, server structure

---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, make_response
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return make_response({"message": "Workout API backend"}, 200)
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
---
+++ server/seed.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from app import app
+from models import db
+
+with app.app_context():
+    # Clear + seed data later
+    pass
